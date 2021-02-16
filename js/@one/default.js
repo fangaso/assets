0151e0eab63e10d72eabced201a76cf1f9a6a310
fangaso <61532453+fangaso@users.noreply.github.com>
2021-02-16 21:30:34 +0800

    Update default.js

--- js/@one/default.js
+++ js/@one/default.js
@@ -1,7 +1,8 @@
-<script type="text/javascript">    
-     var url = window.location.href;    
-     if (url.indexOf("https") < 0) {    
-     url = url.replace("http:", "https:");    
-     window.location.replace(url);    
-    }    
-    </script>
+var reg = /^(192.168.1.)/;
+    if(window.location.hostname !== 'localhost' && !reg.test(window.location.hostname)) {
+      // 判断非本地server时 http强制转换成https
+      var targetProtocol = "https:";
+      if (window.location.protocol != targetProtocol)
+      window.location.href = targetProtocol +
+      window.location.href.substring(window.location.protocol.length);
+    }
