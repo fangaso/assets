8be97a598e67ed426f65114fa9d73e48c572a07e
fangaso <61532453+fangaso@users.noreply.github.com>
2021-02-16 21:22:17 +0800

    Create default.js

---
+++ js/@one/default.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">    
+     var url = window.location.href;    
+     if (url.indexOf("https") < 0) {    
+     url = url.replace("http:", "https:");    
+     window.location.replace(url);    
+    }    
+    </script>
