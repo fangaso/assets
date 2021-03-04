20567a8e3721490b22b6c305623dd0cb5ca03ea7
fangaso <61532453+fangaso@users.noreply.github.com>
2021-03-04 23:17:33 +0800

    Delete js/@one directory

--- js/@one/default.js
+++
@@ -1,8 +0,0 @@
-var reg = /^(192.168.1.)/;
-    if(window.location.hostname !== 'localhost' && !reg.test(window.location.hostname)) {
-      // 判断非本地server时 http强制转换成https
-      var targetProtocol = "https:";
-      if (window.location.protocol != targetProtocol)
-      window.location.href = targetProtocol +
-      window.location.href.substring(window.location.protocol.length);
-    }
